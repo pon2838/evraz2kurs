b2c54e6037756453ce8e4f15e25086c1b2675ddf
oden <zahvatkinsemen218@gmail.com>
2023-12-05 17:52:13 +0500

    работаю по css

---
+++ задачи/задача1.js
@@ -0,0 +1,16 @@
+// const input = document.createElement('input');
+// input.type = 'text';
+// docume
+
+
+
+let aaa='а а б в'
+aaa.length;
+aaa[aaa.length-1];
+// const input = document.getElementById('input-id');
+// const value = input.value
+//
+//  var inputValue = document.getElementById('myInput').value;
+// class input {
+//
+// }
